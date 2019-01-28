d461b4e7a1974877dd804eac6df5221007cc7a14
Francesca Grisoni <38946597+grisoniFr@users.noreply.github.com>
2019-01-28 22:45:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='whales',
       version='1.0',
       description='WHALES molecular descriptors calculation',
-      url='http://github.com/xxx',
+      url='http://github.com/whales_descriptors',
       author='Francesca Grisoni',
-      author_email='francesca.grisoni@unimib.it',
+      author_email='francesca.grisoni@pharma.ethz.ch',
       license='Creative Commons Attribution-NonCommercial 4.0 International License. ',
       packages=['whales_descriptors'],
       zip_safe=False)
